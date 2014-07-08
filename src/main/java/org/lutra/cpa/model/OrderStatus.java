ec08cfd8760e04ffa13a4238ada841cbe5910f2c
Dmitry V <savraz@gmail.com>
2014-07-08 15:52:59 +0400

    colouring of rows

--- src/main/java/org/lutra/cpa/model/OrderStatus.java
+++ src/main/java/org/lutra/cpa/model/OrderStatus.java
@@ -2,18 +2,20 @@ package org.lutra.cpa.model;
 
 public enum OrderStatus
 {
-    UNPAID    ("Заказ оформлен, но еще не оплачен", "Не оплачен"),
-    PROCESSING("Заказ можно выполнять", "Выполнен"),
-    CANCELLED ("Заказ отменен", "Отменен"),
-    DELIVERY  ("Заказ передан в доставку", "Доставка"),
-    PICKUP    ("Заказ доставлен в пункт самовывоза", "Самовывоз"),
-    DELIVERED ("Заказ получен покупателем", "Выполнен");
+    UNPAID    ("Заказ оформлен, но еще не оплачен", "Не оплачен", "warning"),
+    PROCESSING("Заказ можно выполнять", "Выполняется", "danger"),
+    CANCELLED ("Заказ отменен", "Отменен", ""),
+    DELIVERY  ("Заказ передан в доставку", "Доставка", "warning"),
+    PICKUP    ("Заказ доставлен в пункт самовывоза", "Самовывоз", "warning"),
+    DELIVERED ("Заказ получен покупателем", "Выполнен", "success");
 
     final String desc;
     final String brief;
-    OrderStatus(String desc, String brief)
+		final String css_class;
+    OrderStatus(String desc, String brief, String css_class)
     {
         this.desc = desc;
         this.brief = brief;
+				this.css_class = css_class;
     }
 }
