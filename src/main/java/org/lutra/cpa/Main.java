121af9410cb94d809843f0eaf80cc83b1340000f
Dmitry V <savraz@gmail.com>
2014-07-25 18:08:46 +0400

    extracted wrapper

--- src/main/java/org/lutra/cpa/Main.java
+++ src/main/java/org/lutra/cpa/Main.java
@@ -1,87 +1,21 @@
 package org.lutra.cpa;
 
-import com.google.gson.*;
 import org.joda.time.DateTime;
 import org.lutra.cpa.model.Outlet;
 import org.lutra.cpa.service.OutletsService;
+import org.lutra.cpa.wrapper.GsonWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Type;
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 public class Main
 {
 	private static Logger log = LoggerFactory.getLogger(Main.class);
-	public static GsonWrapper g = new GsonWrapper();
-	public static class GsonWrapper
-	{
-		private static Logger log = LoggerFactory.getLogger("G:");
-		private static Gson gson = setUpGson();
-		GsonWrapper()
-		{
-			this.gson = setUpGson();
-		}
-
-		public static Gson setUpGson()
-		{
-			GsonBuilder gb = new GsonBuilder();
-			gb.registerTypeAdapter
-				(
-					Date.class, new JsonSerializer<Date>()
-					{
-						@Override
-						public JsonElement serialize(Date src, Type typeOfSrc, JsonSerializationContext context)
-						{
-							return new JsonPrimitive(fmt.format(src));
-						}
-					}
-				);
-			gb.registerTypeAdapter
-				(
-					Date.class, new JsonDeserializer<Date>()
-					{
-						@Override
-						public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException
-						{
-							Date d = null;
-							try
-							{
-								d = json == null ? null : fmt.parse(json.getAsString());
-							}
-							catch(ParseException e)
-							{
-							}
-							return d;
-						}
-					}
-				);
-
-			return gb.create();
-		}
-
-		public <T> T fromJson(String json, Class<T> classOfT) throws JsonSyntaxException
-		{
-			log.info("<{}> f: {}", classOfT, json);
-
-			return gson.fromJson(json, classOfT);
-		}
-
-		public String toJson(Object src)
-		{
-			String ret = gson.toJson(src);
-			log.info("<{}> t: {}", src.getClass(), ret);
-
-			return  ret;
-		}
-
-	}
-
 	final public static DateFormat fmt = new SimpleDateFormat("dd-MM-yyyy");
+	public static GsonWrapper g = GsonWrapper.i();
 
 	public static void main(String[] args)
 	{
---
+++ src/main/java/org/lutra/cpa/wrapper/GsonWrapper.java
@@ -0,0 +1,89 @@
+package org.lutra.cpa.wrapper;
+
+import com.google.gson.*;
+import org.lutra.cpa.Main;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.util.Date;
+
+/**
+ * 25.07.2014 at 18:04
+ * GsonWrapper of market-cpa project
+ *
+ * @author Dmitry V. (savraz [at] gmail.com)
+ */
+public class GsonWrapper
+{
+	private static Logger log = LoggerFactory.getLogger("G:");
+	private static Gson gson = setUpGson();
+	private static GsonWrapper instance;
+
+	GsonWrapper()
+	{
+		gson = setUpGson();
+	}
+
+	public static GsonWrapper i()
+	{
+		if(instance == null)
+			instance = new GsonWrapper();
+
+		return instance;
+	}
+
+	public static Gson setUpGson()
+	{
+		GsonBuilder gb = new GsonBuilder();
+		gb.registerTypeAdapter
+			(
+				Date.class, new JsonSerializer<Date>()
+				{
+					@Override
+					public JsonElement serialize(Date src, Type typeOfSrc, JsonSerializationContext context)
+					{
+						return new JsonPrimitive(Main.fmt.format(src));
+					}
+				}
+			);
+		gb.registerTypeAdapter
+			(
+				Date.class, new JsonDeserializer<Date>()
+				{
+					@Override
+					public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException
+					{
+						Date d = null;
+						try
+						{
+							d = json == null ? null : Main.fmt.parse(json.getAsString());
+						}
+						catch(ParseException e)
+						{
+						}
+						return d;
+					}
+				}
+			);
+
+		return gb.create();
+	}
+
+	public <T> T fromJson(String json, Class<T> classOfT) throws JsonSyntaxException
+	{
+		log.info("<{}> f: {}", classOfT, json);
+
+		return gson.fromJson(json, classOfT);
+	}
+
+	public String toJson(Object src)
+	{
+		String ret = gson.toJson(src);
+		log.info("<{}> t: {}", src.getClass(), ret);
+
+		return  ret;
+	}
+
+}
